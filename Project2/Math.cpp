8b88541e4659ea1a8ec90389f6a5e7dde9fd769b
Narayan-21 <naaidjan.19@gmail.com>
2024-03-30 16:06:31 +0530

    variables in cpp

--- Project2/Math.cpp
+++ Project2/Math.cpp
@@ -9,6 +9,12 @@ static int Multiply(int a, int b)
 
 int main()
 {
-	std::cout << Multiply(5, 9) << std::endl;
+	//char -> 1byte, short -> 2 bytes, int -> 4 bytes,, long -> 4 bytes (depending on the compiler), long long  -> 8 bytes of data.
+	// float -> 4 bytes, double -> 8 bytes.
+	// bool -> true/false (0 is false, any other number is true) -> 1 bytes
+	unsigned int variable = 8; //-2b -> 2b, but with unsigned it is : 0 -> 4b
+	float var = 5.5f;
+	double var2 = 2.316;
+	std::cout << sizeof(var2) << std::endl;
 	std::cin.get();
 }
